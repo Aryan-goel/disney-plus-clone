3d79e15f3eb56f17608c86a4de1748132a3e57fc
91639 <goel.aryan2406@gmail.com>
2021-12-04 01:18:06 +0530

    nav bar content

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,30 @@ function header() {
     <Nav>
       <Logo src="/images/logo.svg" />
       <NavMenu>
-          <a>
-            <img src="/images/home-icon.svg"/>
-            <span>
-                Home
-            </span>
-
-          </a>
-
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
       </NavMenu>
     </Nav>
   );
@@ -23,23 +39,25 @@ export default header;
 const Nav = styled.nav`
   height: 70px;
   background: #090b13;
-  display:flex;
-  align-items:center;
-  padding:0 36px;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
 `;
 const Logo = styled.img`
- width:80px;
- 
- 
- `;
-const NavMenu=styled.div`
-display:flex;
-a{
-    display:flex;
-    align-items:center;
-    padding:0 12px;
-    img{
-        
+  width: 80px;
+`;
+const NavMenu = styled.div`
+  display: flex;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    img {
+      height: 20px;
     }
-}
-`
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+    }
+  }
+`;
