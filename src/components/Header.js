2ebb087b207ebdf9ca2e4dac6c5929df05a3e8a5
91639 <goel.aryan2406@gmail.com>
2021-12-05 17:52:06 +0530

    header animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -76,7 +76,17 @@ const NavMenu = styled.div`
           left:0;
           right:0;
           bottom:-6px;
+           opacity:0;
+           transform-origin: left center;
+           transition:all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+          transform:scaleX(0);
 
+      }
+    }
+    &:hover{
+      span:after{
+      transform:scaleX(1);
+      opacity:1;
       }
     }
   }
