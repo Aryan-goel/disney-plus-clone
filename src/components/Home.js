137e60065231b9df1cda84cefb0e6451eff66c4a
91639 <goel.aryan2406@gmail.com>
2021-12-06 22:11:34 +0530

    background added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Home() {
-    return (
-        <div>
-            home
-        </div>
-    )
+  return <Container>
+
+Home
+  </Container>;
 }
 
-export default Home
+export default Home;
+
+const Container = styled.main`
+min-height:calc(100vh - 70px);
+padding:0 calc(3.5wh + 5px);
+position:relative;
+
+&:before {
+  background:url("/images/home-background.png") center center/
+ cover no-repeat fixed;
+ content:"";
+ position:absolute;
+ top:0;
+ left:0;
+ right:0;
+ bottom:0;
+
+
+}
+
+`;
