cb9d0bb2a288e538cab913e93e12f9de80f083e7
91639 <goel.aryan2406@gmail.com>
2021-12-07 20:23:07 +0530

    overflow fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,6 +17,7 @@ const Container = styled.main`
 min-height:calc(100vh - 70px);
 padding:0 calc(3.5wh + 5px);
 position:relative;
+overflow-x:hidden;
 
 &:before {
   background:url("/images/home-background.png") center center/
