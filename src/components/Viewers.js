c6fad1207f18072f7a30db01381c449ddb38dec3
91639 <goel.aryan2406@gmail.com>
2021-12-08 22:37:35 +0530

    home page UI design done

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -8,16 +8,16 @@ function Viewers() {
         <img src="/images/viewers-disney.png"  alt=""/>
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-pixar.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-marvel.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-starwars.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-national.png" />
       </Wrap>
     </Container>
   );
