7de09d8d30644065385f739e13490025710fc171
91639 <goel.aryan2406@gmail.com>
2021-12-08 22:51:38 +0530

    react-router-dom init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
-      <Header/> 
-      <Home/>
+      <Router>
+      <Header />
+      <Switch>
+        <Route path="/detail">
+        <Detail/>
+        </Route>
+        
+          <Route path="/">
+           <Home></Home>
+        </Route>
+      </Switch>
+      </Router>
+    
     </div>
   );
 }
- 
+
 export default App;
