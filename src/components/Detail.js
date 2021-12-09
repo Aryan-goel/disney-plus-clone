00db31ba1b04db7e633a16cec2f85863fd8e61b7
91639 <goel.aryan2406@gmail.com>
2021-12-09 10:35:31 +0530

    buttons styling

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -11,19 +11,16 @@ function Detail() {
         <img src="https://static1.squarespace.com/static/51cdafc4e4b09eb676a64e68/t/5ac4f08a6d2a73ba32fd68da/1540496405918/bau_poster.jpg" />
       </ImageTitle>
       <Controls>
-          <PlayButton>
-            <img src="/images/play-icon-black.png"/>
-            <span> Play</span>
-          </PlayButton>
-          <Trailer>
-
-          </Trailer>
-          <AddButton>
-
-          </AddButton>
-          <Groupwatch>
-
-          </Groupwatch>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span> Play</span>
+        </PlayButton>
+        <Trailer>
+          <img src="/images/play-icon-white.png" />
+          <span> Trailer</span>
+        </Trailer>
+        <AddButton></AddButton>
+        <Groupwatch></Groupwatch>
       </Controls>
     </Containter>
   );
@@ -62,13 +59,30 @@ const ImageTitle = styled.div`
   }
 `;
 const Controls = styled.div`
- 
+  display: flex;
 `;
-const PlayButton=styled.button`
-`
-const  Trailer=styled.button`
-`
-const AddButton=styled.button`
-`
-const Groupwatch=styled.button`
-`
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding:0 24px;
+  margin-right:22px;
+  display: flex;
+  align-items: center;
+  height:56px;
+  background:rgb(249,249,249);
+  border:none;
+  letter-spacing:1.8px;
+  cursor:pointer;
+
+  &:hover{
+  background:rgb(198,198,198);
+  }
+`;
+const Trailer = styled(PlayButton)`
+background:rgb(0,0,0,0.3);
+border:1px solid rgb(249,249,249);
+color:rgb(249,249,249);
+text-transform:uppercase;
+`;
+const AddButton = styled.button``;
+const Groupwatch = styled.button``;
