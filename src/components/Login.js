c35e0b8944f9a80c9c3e1c98d65b4e86bcf4469b
91639 <goel.aryan2406@gmail.com>
2021-12-09 19:04:32 +0530

    login finished

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+  return (
+    <Container>
+      <CTA>
+        <CTALogoOne src="/images/cta-logo-one.svg" />
+        <SignUp>Get all there!</SignUp>
+        <Description>
+        © 2021 STAR. All Rights Reserved. HBO, Home Box Office and all related channel and programming logos are service marks of, and all related programming visuals and elements are the property of, Home Box Office, Inc. All rights reserved.
+        </Description>
+        <CTALogoTwo src="/images/cta-logo-two.png" />
+      </CTA>
+
+    </Container>
+  );
 }
 
-export default Login
+export default Login;
+const Container = styled.div`
+  position: relative;
+  height: calc(100vh - 70px);
+  display: flex;
+  justify-content: center;
+  align-items: top;
+
+  &:before {
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("/images/login-background.jpg");
+    position: absolute;
+    content: "";
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    opacity:0.7;
+    z-index: -1;
+  }
+`;
+const CTA = styled.div`
+  max-width: 650px;
+  padding: 80px 40x;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  margin-top:100px;
+`;
+const CTALogoOne = styled.img``;
+const SignUp = styled.div`
+  width: 100%;
+  background-color: #0063e5;
+  font-weight: bold;
+  padding: 18px 0;
+  color: #f9f9f9;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 18px;
+  cursor: pointer;
+  transition: all 250ms;
+  letter-spacing: 1.5px;
+  margin-top: 8px;
+  margin-bottom: 12px;
+  &: hover {
+    background: #0483ee;
+  }
+`;
+const Description=styled.p`
+font-size:11px;
+letter-spacing:1.5px;
+text-align:center;
+line-height:1.5;
+`
+const CTALogoTwo=styled.img`
+
+padding 0 15px;
+`
